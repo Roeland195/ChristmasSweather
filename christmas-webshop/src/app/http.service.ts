7fe3cabc0f370f046c2e28244213d4e3cfc65a1c
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2022-03-31 20:15:45 +0200

    Revert "Update http.service.ts"
    This reverts commit 3e08642bcbb121b9d059a5a3f47e9c55390894a5.

--- christmas-webshop/src/app/http.service.ts
+++ christmas-webshop/src/app/http.service.ts
@@ -11,9 +11,7 @@ export class HttpSercive{
     private authenticated = false;
 
     // private url: string = "https://still-forest-71875.herokuapp.com";
-    // private url: string = "http://localhost:8080";
-    private url: string = "http://testchristmassweather.herokuapp.com";
-
+    private url: string = "http://localhost:8080";
 
     private http : HttpClient;
 
